7f91239c883a96550b558ec27aec93dc5bb2511f
eswaribala <Parameswaribala@gmail.com>
2020-02-04 17:03:46 +0530

    Update menudata.ts

--- menudata.ts
+++ menudata.ts
@@ -6,6 +6,11 @@ let menuData:Menu[]=[new Menu('Account Summary','','',
 
   new Menu('Requests','','',
     [new SubMenu('Cheque Request','',''),
-    new SubMenu('Address Change','','')])
+    new SubMenu('Address Change','','')]),
+
+  new Menu('Fund Transfer','','',
+    [new SubMenu('NEFT','',''),
+      new SubMenu('RTGS','',''),
+      new SubMenu('IMPS','','')])
 
 ]
